c58c3df4b9e47072739c57f42d9f0b588592331d
André Verweij <51133@gmail.com>
2025-02-10 14:00:55 +0100

    added a display instance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		Display window = new Display();
+		System.out.println("Created a new display");
 	}
 }
